50c6de0d1fc86c5fcd5c9da88a64fd9afb0c8cc8
arisohib <92259742+arisohib@users.noreply.github.com>
2023-08-02 08:20:32 +0700

    update profile ari

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
+import pnbppkh from "../../Assets/Projects/KLHK.PNG";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import manlab from "../../Assets/Projects/MANLAB.PNG";
+import simonika from "../../Assets/Projects/SIMONIKA.PNG";
+import serviskita from "../../Assets/Projects/serviskita.PNG";
+import sitaruboyo from "../../Assets/Projects/SITARUBOYO.PNG";
 
 function Projects() {
   return (
@@ -23,70 +23,59 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={simonika}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="SIMONIKA"
+              description="Development of the SIMONIKA Application (Sistem Monitoring Kualitas Air) owned by the East Java Provincial Government. In this application, a system is created to accommodate all data on the location of bridges or rivers in East Java. This is intended so that the East Java Provincial Government has river water data. the data will be processed which will later be used as a parameter to measure the level of pollution in the river and that location. by using predefined parameters such as microbiology, chemistry, and physics. which is calculated using STORET Standard and IP Standard."
+              ghLink="https://github.com/arisohib"
+              // demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={sitaruboyo}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="SITARUBOYO"
+              description="The application belongs to the Surabaya city government, namely an information system which is a combination of projects. a combination of GIS Information Systems and Laravel Applications. This application has 2 types of data, namely tabular data for Laravel applications. and spatial data for use in GIS information systems. database used Postgresql."
+              ghLink="https://github.com/arisohib"
+              demoLink="https://sitaruboyo.dprkpp.web.id/index/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={manlab}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="MANLAB"
+              description="Clinical application for the online registration process, PCR Swab registration, print registration cards, print lab test results, and various medical checkups and their prices"
+              ghLink="https://github.com/arisohib"
+              // demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={pnbppkh}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="PNBP-PKH"
+              description="KLHK's application (Ministry of Environment and Forestry) for an information system that handles payment processes and correspondence to payers. and also for payment reports such as accounts payable, income, payment data generation, automatic letter generation, and digital signatures."
+              ghLink="https://github.com/arisohib"
+              demoLink="https://pnbp-pkh2.menlhk.go.id/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={serviskita}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Serviskita.com"
+              description="This application is an online shuttle service. the purpose of making this application is to make it easier for services that can be done from home. This application was built using the Laravel framework, MySQL. with a multi-role flow scheme. each user will have a member area and implement the SSO (Single Sign On) system."
+              ghLink="https://github.com/arisohib"
+              demoLink="https://apps.serviskita.com/login" 
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
         </Row>
       </Container>
     </Container>
